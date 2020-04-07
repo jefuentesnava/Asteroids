fb9dfdfccf029ae5bc1a55776e8b0ecac5edc13e
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-06 23:14:41 -0700

    Spawn four large asteroids with random movement vectors

---
+++ Assets/Scripts/LargeAsteroid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LargeAsteroid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.eulerAngles = new Vector3(0f, 0f, Random.Range(0, 360));
+        GetComponent<Rigidbody2D>().AddForce(transform.up * 50);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO
+    }
+}
