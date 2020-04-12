dbcc7a38f5b7c0b43f53d6fd896711edc4e076a2
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-11 23:25:35 -0700

    Remove deprecated scripts

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -7,11 +7,12 @@ public class Ship : MonoBehaviour
     private int extraLives = 3;
     private float thrusterSpeed = 5f;
     private float rotationSpeed = 1f;
+    Vector3 originalPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        originalPosition = gameObject.transform.position;
     }
 
     // Update is called once per frame
@@ -32,9 +33,39 @@ public class Ship : MonoBehaviour
             transform.Rotate(-Vector3.forward * rotationSpeed);
         }
 
+        
+
     }
+
     public int getExtraLives()
     {
         return extraLives;
+    }
+
+    //called when Ship collides with an asteroid
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Asteroid")
+        {
+            Debug.Log("Asteroid collision detected");
+        }
+
+        if (extraLives > 0)
+        {
+            Debug.Log("Respawning...");
+            respawn();
+        }
+        
+    }
+
+    private IEnumerator respawn()
+    {
+        extraLives--;
+
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(1);
+
+        gameObject.transform.position = originalPosition;
+        gameObject.SetActive(true);
     }
 }
