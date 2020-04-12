e1ed25ac26d8c7ffa97e7c4febcb44b7f7195ca2
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-12 02:29:51 -0700

    Redirect user to game over screen after death without extra lives

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    public void getUserName()
+    {
+        Debug.Log(transform.Find("Name Input").GetComponent<TMP_InputField>().text);
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
