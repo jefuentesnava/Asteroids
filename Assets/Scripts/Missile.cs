c3c71f3639c2fd6acfb0bfb767fe1b7a251e0658
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-14 00:32:03 -0700

    Add missile screen wrapping, timeout

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
+    private float TimeOut = 0.25f;
+    
     public float missileSpeed = 20f;
     public Rigidbody2D rigidBody;
+    bool isWrappingHorizontally = false;
+    bool isWrappingVertically = false;
+    private float timer = 0.0f;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidBody.velocity = transform.up * missileSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenWrap();
+        timer += Time.deltaTime;
+
+        if (timer > TimeOut)
+        {
+            Destroy(gameObject);
+        }
     }
 
     //missile-asteroid collision behavior; called when missile collides with asteroid
@@ -20,5 +37,57 @@ public class Missile : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+        //screen-wrapping functions
+    private bool isVisible()
+    {
+        Renderer renderer = GetComponentInChildren<Renderer>();
+
+        if (renderer.isVisible)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void screenWrap()
+    {
+        Camera camera = Camera.main;
+        Vector3 viewportPosition = camera.WorldToViewportPoint(transform.position);
+        Vector3 newPosition = transform.position;
+
+        //if ship is visible, no wrapping is necessary
+        if (isVisible())
+        {
+            isWrappingHorizontally = false;
+            isWrappingVertically = false;
+            return;
+        }
+
+        //if ship is currently wrapping, do nothing
+        if (isWrappingHorizontally && isWrappingVertically)
+        {
+            return;
+        }
+
+        //detect whether ship has disappeared horizontally...
+        if (!isWrappingHorizontally && (viewportPosition.x > 1 || viewportPosition.x < 0))
+        {
+            newPosition.x = -newPosition.x;
+            isWrappingHorizontally = true;
+        }
+
+        //...or vertically
+        if (!isWrappingVertically && (viewportPosition.y > 1 || viewportPosition.y < 0))
+        {
+            newPosition.y = -newPosition.y;
+            isWrappingVertically = true;
+        }
+
+        transform.position = newPosition;
     }
 }
