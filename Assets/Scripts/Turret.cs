ed956759a21c375d79338a9369a0f9fa7d78ebac
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-16 21:50:29 -0700

    Refactor turret
    Set FiringPoint as parent of each missile

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -18,6 +18,6 @@ public class Turret : MonoBehaviour
 
     void Shoot()
     {
-        Instantiate(missilePrefab, firingPoint.position, firingPoint.rotation);
+        Instantiate(missilePrefab, firingPoint.position, firingPoint.rotation, firingPoint);
     }
 }
