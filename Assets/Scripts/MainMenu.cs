11455a741aec38f52f2441ae9fa3e293c673afc9
Patrick Silvestre <patrickjohnsilvestre@gmail.com>
2020-04-19 07:19:21 -0700

    Reset global state before playthrough

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartSinglePlayerMode()
     {
+        GlobalState.instance.reset();
         SceneManager.LoadScene("Level01", LoadSceneMode.Single);
     }
 }
