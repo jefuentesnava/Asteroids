8272c9a6f0f22835fa96fdc1f13a52617f58e37b
Jesus Emmanuel Fuentes Nava <44213746+jefuentesnava@users.noreply.github.com>
2020-04-29 20:44:03 -0700

    Ship Bullet Sound
    Implemented Ship Bullet Sound

---
+++ Assets/Scripts/SaucerBullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaucerBullet : MonoBehaviour
+{
+    public AudioSource Saucer_Bullet;
+
+    //plays saucer bullet sound when gameObject is visible
+    private void OnBecameVisible()
+    {
+        Saucer_Bullet.Play();
+    }
+}
